c90e11fb8c0510f4f2f7389923ab9c923598e948
flavio1917 <flavio1917@hotmail.it>
2026-02-12 10:23:32 +0100

    Aggiunta loghi e disclaimer amazon

--- air-fryer/astro.config.mjs
+++ air-fryer/astro.config.mjs
@@ -1,17 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  // FONDAMENTALE PER LA SITEMAP
+  site: 'https://www.convertitorefriggitrice.it',
+
   i18n: {
     defaultLocale: 'it',
     locales: ['it', 'en', 'es'],
     routing: {
-      // CAMBIA DA TRUE A FALSE:
       prefixDefaultLocale: false 
     }
   },
+
+  integrations: [sitemap()],
+
   vite: {
     plugins: [tailwindcss()]
   }
