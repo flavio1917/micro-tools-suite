d61e57da220b19fea85d6c9bb5801c6d0f47d3e1
flavio1917 <flavio1917@hotmail.it>
2026-02-12 10:35:26 +0100

    FIX FINALE: Inserite dipendenze sitemap nel package.json

--- air-fryer/astro.config.mjs
+++ air-fryer/astro.config.mjs
@@ -1,13 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-  // FONDAMENTALE PER LA SITEMAP
   site: 'https://www.convertitorefriggitrice.it',
-
   i18n: {
     defaultLocale: 'it',
     locales: ['it', 'en', 'es'],
@@ -15,9 +11,13 @@ export default defineConfig({
       prefixDefaultLocale: false 
     }
   },
-
-  integrations: [sitemap()],
-
+  integrations: [
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+    })
+  ],
   vite: {
     plugins: [tailwindcss()]
   }
