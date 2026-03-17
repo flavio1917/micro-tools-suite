b642662db66d4048e73a4fbe50a76be8e97fed08
flavio1917 <flavio1917@hotmail.it>
2026-03-17 15:59:44 +0100

    stelline v2

--- air-fryer/astro.config.mjs
+++ air-fryer/astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 import vercel from '@astrojs/vercel';
 export default defineConfig({
   site: 'https://www.convertitorefriggitrice.it',
-  
+  output: 'static',
   // output: 'hybrid', 
   adapter: vercel(), 
 
