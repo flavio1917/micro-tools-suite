d10e195adacabd483565b4545ef3310312d66a44
flavio1917 <flavio1917@hotmail.it>
2026-03-18 15:59:22 +0100

    fix static v3

--- air-fryer/astro.config.mjs
+++ air-fryer/astro.config.mjs
@@ -6,7 +6,7 @@ import vercel from '@astrojs/vercel';
 export default defineConfig({
   site: 'https://www.convertitorefriggitrice.it',
   
-  output: 'hybrid', // <--- LA MAGIA È QUI
+  output: 'static', // <--- Torna a static!
   adapter: vercel(), 
 
   i18n: {
