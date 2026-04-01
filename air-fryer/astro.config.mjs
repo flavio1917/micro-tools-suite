043b0edae00d1eb59116388ea3faf1707d772e65
flavio1917 <flavio1917@hotmail.it>
2026-04-01 12:49:56 +0200

    modifica conversione ita

--- air-fryer/astro.config.mjs
+++ air-fryer/astro.config.mjs
@@ -1,39 +1,39 @@
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel';
+  import { defineConfig } from 'astro/config';
+  import tailwindcss from '@tailwindcss/vite';
+  import sitemap from '@astrojs/sitemap';
+  import vercel from '@astrojs/vercel';
 
-export default defineConfig({
-  site: 'https://www.convertitorefriggitrice.it',
-  
-  output: 'static', // <--- Torna a static!
-  adapter: vercel(), 
+  export default defineConfig({
+    site: 'https://www.convertitorefriggitrice.it',
+    
+    output: 'static', // <--- Torna a static!
+    adapter: vercel(), 
 
-  i18n: {
-    defaultLocale: 'it',
-    locales: ['it', 'en', 'es', 'fr'],
-    routing: {
-      prefixDefaultLocale: false 
-    }
-  },
-  integrations: [
-    sitemap({
-      changefreq: 'weekly',
-      priority: 0.7,
-      lastmod: new Date(),
-      i18n: {
-        defaultLocale: 'it',
-        locales: {
-          it: 'it',
-          en: 'en',
-          es: 'es',
-          fr: 'fr',
+    i18n: {
+      defaultLocale: 'it',
+      locales: ['it', 'en', 'es', 'fr'],
+      routing: {
+        prefixDefaultLocale: false 
+      }
+    },
+    integrations: [
+      sitemap({
+        changefreq: 'weekly',
+        priority: 0.7,
+        lastmod: new Date(),
+        i18n: {
+          defaultLocale: 'it',
+          locales: {
+            it: 'it',
+            en: 'en',
+            es: 'es',
+            fr: 'fr',
+          },
         },
-      },
-    })
-  ],
-  vite: {
-    plugins: [tailwindcss()]
-  },
-  prefetch: true
-});
+      })
+    ],
+    vite: {
+      plugins: [tailwindcss()]
+    },
+    prefetch: true
+  });
